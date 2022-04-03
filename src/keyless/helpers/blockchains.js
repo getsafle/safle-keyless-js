bc3cce9d7d4e660c17babb31c350c4d7fb3d28f9
Sebastian Bratu <artweb11@gmail.com>
2022-04-04 02:04:34 +0300

    send transaction from web3

--- src/keyless/helpers/blockchains.js
+++ src/keyless/helpers/blockchains.js
@@ -1,19 +1,23 @@
 const blockchainInfo = {
     1: {
         name: 'Ethereum Mainnet',
-        uri: 'https://mainnet.infura.io/v3/'
+        uri: 'https://mainnet.infura.io/v3/',
+        chain_name: 'ethereum',
     },
     3: {
         name: 'Ropsten Testnet',
-        uri: 'https://ropsten.infura.io/v3/'
+        uri: 'https://ropsten.infura.io/v3/',
+        chain_name: 'ropsten',
     },
     137: {
         name: 'Polygon Mainnet',
-        uri: 'https://polygon-mainnet.infura.io/v3/'
+        uri: 'https://polygon-mainnet.infura.io/v3/',
+        chain_name: 'polygon',
     },
     80001: {
         name: 'Mumbai Testnet',
-        uri: 'https://polygon-mumbai.infura.io/v3/'
+        uri: 'https://polygon-mumbai.infura.io/v3/',
+        chain_name: 'mumbai',
     }
 }
 
