20ec50546e827ae5a7c7eac542746e632f1db442
Arjun <arjun1709@gmail.com>
2022-09-27 10:57:49 +0530

    removed support to kovan, rinkeby and goerli

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const safleBlockchainController = require('@getsafle/blockchain-controller');
 
 const getNetworks = async () => {
     const networks = await fetch( config.networks ).then(e=>e.json());
-    const allowedIds = [ 1, 3, 42, 4, 420, 137, 80001 ];
+    const allowedIds = [ 1, 3, 137, 80001 ];
     return Object.values( networks ).reduce( (acc, e) => acc.concat( e ), [] ).filter( e=> allowedIds.indexOf( e.chainId ) != -1 );
 
     return networks;
